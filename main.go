e63d05886b0ff185b325ff02620e03befd8c066d
Eugene Ma <ema@twitter.com>
2016-02-15 12:28:06 -0800

    Set analysis.PantsRoot via flag

--- main.go
+++ main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 
+	"github.com/edma2/pantsindex/analysis"
 	"github.com/edma2/pantsindex/watch"
 )
 
+var (
+	pantsRootFlag = flag.String("root", "", "pants root directory")
+)
+
 func main() {
+	flag.Parse()
+
+	if *pantsRootFlag == "" {
+		if analysis.PantsRoot = os.Getenv("PANTSROOT"); analysis.PantsRoot == "" {
+			cwd, _ := os.Getwd()
+			analysis.PantsRoot = cwd
+		}
+	} else {
+		analysis.PantsRoot = *pantsRootFlag
+	}
+
 	stop := make(chan bool)
-	pathChanges := watch.PathChanges("/Users/ema/src/source/.pants.d/compile/zinc/", stop)
+	pathChanges := watch.PathChanges(analysis.PantsRoot+"/.pants.d/compile/zinc/", stop)
 	analysisFileChanges := watch.AnalysisFileChanges(pathChanges)
 	go func() {
 		for path := range analysisFileChanges {
