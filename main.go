c7b62b2d16edf385c26e568c3dce0dfc1f994e1c
Eugene Ma <ema@twitter.com>
2016-02-15 17:59:31 -0800

    serve plumber

--- main.go
+++ main.go
@@ -3,30 +3,49 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
-	"os"
+	"io"
+	"log"
+	"os/exec"
 	"path"
+
+	"9fans.net/go/plan9"
+	"9fans.net/go/plumb"
 
 	"github.com/edma2/pantsindex/index"
 )
 
 var (
-	root = flag.String("root", ".", "pants root directory")
+	root = flag.String("root", "/Users/ema/src/source/", "pants root directory")
 )
+
+func servePlumber(idx *index.Index, r io.ByteReader) {
+	for {
+		m := plumb.Message{}
+		err := m.Recv(r)
+		if err != nil {
+			log.Println(err)
+		}
+		plumbEdit(idx.Get(string(m.Data)))
+	}
+}
+
+func plumbEdit(path string) {
+	out, err := exec.Command("plumb", "-d", "edit", path).CombinedOutput()
+	if err != nil {
+		log.Fatalf("plumb: %v\n%s", err, out)
+	}
+}
 
 func main() {
 	flag.Parse()
+	plumber, err := plumb.Open("pantsindex", plan9.OREAD)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer plumber.Close()
 
 	idx := index.NewIndex()
 	idx.Watch(path.Join(*root, ".pants.d", "compile", "zinc"))
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "stop" {
-			break
-		}
-		fmt.Println(idx.Get(text))
-	}
-	idx.Stop()
+	servePlumber(idx, bufio.NewReader(plumber))
+	idx.Stop() // not reached
 }
