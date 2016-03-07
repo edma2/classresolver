b9c80ea1b8c017d93825db100bf045f31b7a695b
Eugene Ma <ema@twitter.com>
2016-03-06 20:59:10 -0800

    handle wdir paths starting with "/c/"

--- main.go
+++ main.go
@@ -101,6 +101,9 @@ func serve(idx *index.Index) error {
 		}
 		log.Printf("Received from plumber: %s\n", m)
 		name := string(m.Data)
+		if strings.HasPrefix(m.Dir, "/c/") {
+			name = strings.TrimPrefix(m.Dir, "/c/") + "." + name
+		}
 		var get *index.GetResult
 		for _, c := range candidatesOf(name) {
 			if get = idx.Get(c); get != nil {
